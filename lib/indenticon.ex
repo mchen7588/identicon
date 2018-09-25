e0e0378bea3a71f3b34a6fee1c5f9746695b5e66
Michael Chen <mchen7588@gmail.com>
2018-09-24 23:23:34 -0700

    first commit

---
+++ lib/indenticon.ex
@@ -0,0 +1,18 @@
+defmodule Indenticon do
+  @moduledoc """
+  Documentation for Indenticon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Indenticon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/indenticon_test.exs
@@ -0,0 +1,8 @@
+defmodule IndenticonTest do
+  use ExUnit.Case
+  doctest Indenticon
+
+  test "greets the world" do
+    assert Indenticon.hello() == :world
+  end
+end
