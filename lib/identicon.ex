b4fc8c2a1a60f4c11a749d1444907962046a87c4
Michael Chen <mchen7588@gmail.com>
2018-09-28 19:24:37 -0700

    filter odd squares out of grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,11 +4,13 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
   end
@@ -17,15 +19,27 @@ defmodule Identicon do
     %Identicon.Image{image_struct | color: {r, g, b}}
   end
 
-  def build_grid(%Identicon.Image{hex: hex}) do
-    hex
-    |> Enum.chunk_every(3, 3, :discard)
-    |> Enum.map(&mirror_row/1)
+  def build_grid(%Identicon.Image{hex: hex} = image_struct) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
 
+    %Identicon.Image{image_struct | grid: grid}
   end
 
   def mirror_row([x, y, z]) do
     [x, y, z, y, x]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image_struct) do
+    filtered_grid = Enum.filter grid, fn({value, _index}) ->
+      rem(value, 2) == 0
+    end
+
+    %Identicon.Image{image_struct | grid: filtered_grid}
   end
 
 
